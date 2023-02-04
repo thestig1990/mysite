80518edaa91842cad39c40cd2eae54873c10cfda
thestig1990 <yakymov1geka@gmail.com>
2023-02-04 17:16:05 +0200

    update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 echo "-----------Starting to deploy docker image-----------"
 DOCKER_IMAGE="thestig90/mysite-apache:latest"
+echo "Docker image name - $DOCKER_IMAGE"
 
 echo "-----------Pull docker image-----------"
 docker pull $DOCKER_IMAGE
+echo "-----------Docker image pulled-----------"
 
-echo "-----------Stops docker container if it is running-----------"
+echo "-----------Stops and remove docker container if it is running-----------"
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker ps -a -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker rm
 
+echo "-----------Remove unused docker images with tag 'build-*'----------"
+docker rmi -f $(docker images -q --filter=reference='thestig90/mysite-apache:build*')
+
 echo "-----------Run container with port mapping-----------"
-docker run -dit --name=mysite -p 8081:80 $DOCKER_IMAGE
+CONTAINER_NAME="mysite"
+docker run -dit --name=$CONTAINER_NAME -p 8081:80 $DOCKER_IMAGE
+echo "Container has been successfully launched"
