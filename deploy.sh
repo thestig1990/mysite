132a9594972191d91f307173e8704bc9292a26b3
thestig1990 <yakymov1geka@gmail.com>
2023-02-04 16:15:19 +0200

    update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ docker pull $DOCKER_IMAGE
 
 echo "-----------Stops docker container if it is running-----------"
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+docker ps -a -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker rm
 
 echo "-----------Run container with port mapping-----------"
-docker run -dit -p 8081:80 $DOCKER_IMAGE
+docker run -dit --name=mysite -p 8081:80 $DOCKER_IMAGE
