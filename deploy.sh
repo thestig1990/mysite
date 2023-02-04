4507ca5a57cd3028356a75ed50eae1b8c67eb846
thestig1990 <yakymov1geka@gmail.com>
2023-02-04 12:58:31 +0200

    update deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "-----------Starting to deploy docker image-----------"
+DOCKER_IMAGE="thestig90/mysite-apache:latest"
+
+echo "-----------Pull docker image-----------"
+docker pull $DOCKER_IMAGE
+
+echo "-----------Stops docker container if it is running-----------"
+docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+
+echo "-----------Run container with port mapping-----------"
+docker run -dit -p 8081:80 $DOCKER_IMAGE
